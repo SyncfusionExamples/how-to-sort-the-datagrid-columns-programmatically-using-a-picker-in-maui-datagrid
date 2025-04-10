60592f9e957733137be53974d9a98dec64f55988
SethupathyD <sethupathy.d@syncfusion.com>
2025-04-10 16:23:26 +0530

    Code updated.

---
+++ SfDataGridSample/SfDataGridSample/Model/OrderInfo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridSample
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        private int orderID;
+        private string customerID;
+        private string freight;
+        private string country;
+
+        public int OrderID
+        {
+            get { return orderID; }
+            set 
+            { 
+                orderID = value;
+                RaisePropertyChanged(nameof(orderID));
+            }
+        }
+        public string CustomerID
+        {
+            get { return customerID; }
+            set
+            {
+                customerID = value;
+                RaisePropertyChanged(nameof(customerID));
+            }
+        }
+
+        public string Freight
+        {
+            get { return freight; }
+            set
+            {
+                freight = value;
+                RaisePropertyChanged(nameof(freight));
+            }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                RaisePropertyChanged(nameof(country));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if(this.PropertyChanged!=null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SfDataGridSample/SfDataGridSample/ViewModel/ViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Syncfusion.Maui.DataGrid;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridSample
+{
+public class ViewModel : INotifyPropertyChanged
+{
+    private Command clearSelection;
+    public Command ClearSelection
+    {
+        get
+        {
+            return clearSelection;
+        }
+    }       
+
+    private void ClearSelectionCommand(object obj)
+    {
+        var dataGrid = obj as SfDataGrid;
+        dataGrid.SelectionMode = DataGridSelectionMode.None;
+    }
+        
+    OrderInfoRepository order;
+    public ViewModel()
+    {
+        order = new OrderInfoRepository();
+        clearSelection = new Command(ClearSelectionCommand);
+        SetRowstoGenerate(50);
+    }
+
+    private ObservableCollection<OrderInfo> ordersInfo;
+    public ObservableCollection<OrderInfo> OrdersInfo
+    {
+        get { return ordersInfo; }
+        set { this.ordersInfo = value; }
+    }
+
+    public void SetRowstoGenerate(int count)
+    {
+        ordersInfo = order.GetOrderDetails(count);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void RaisePropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+}
